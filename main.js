68c5f04e9dbbbc5887c90abf4dab1aa9d27540de
slameuarroz <99189389+slameuarroz@users.noreply.github.com>
2023-03-15 17:13:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function setup() {
+    canvas = createCanvas(280, 240);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+}
+function draw(){;
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY)
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotResults);
+}
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results);
+        var result = results[0].label;
+        document.getElementById("label").innerHTML="nome: " + result.replace("_"," ");
+        document.getElementById("confidence").innerHTML="precisão: " + Math.round(results[0].confidence * 100 ) + "%";
+        utterThis = new SpeechSynthesisUtterance(result.replace('_', ' '));
+        synth.speak(utterThis);
+    }
+    
+}
+function clearCanvas(){
+    background("white");
+}
